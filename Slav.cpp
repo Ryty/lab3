d96ffd199f015b65e7ef50857856bd3b25d19fef
Ryty <225980@student.pwr.edu.pl>
2016-04-01 10:10:20 +0200

    Update Slav.cpp

--- Slav.cpp
+++ Slav.cpp
@@ -18,7 +18,7 @@ void Slav::init()
 
 Slav::Slav()
 {
-	static int amountOfNames = (init(), names.size());
+	static int amountOfNames = (init(), names.size()); //leniwa inicializacja
 	_name = names[rand() % amountOfNames];
 	_id = _counter++;
 }
